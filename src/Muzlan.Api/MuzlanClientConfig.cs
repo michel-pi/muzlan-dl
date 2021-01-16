d63b927a625522a300206d35a49195c7b896c639
Michel <github@michel-pi.de>
2021-01-16 19:17:51 +0100

    working on basic functionality

---
+++ src/Muzlan.Api/MuzlanClientConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Muzlan.Api
+{
+    public class MuzlanClientConfig
+    {
+        public const string DefaultHost = "https://muzlan.top";
+        public const string DefaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0";
+
+        public static readonly MuzlanClientConfig Default = new MuzlanClientConfig();
+
+        public string Host { get; set; }
+        public string UserAgent { get; set; }
+
+        public string? NetworkUsername { get; set; }
+        public string? NetworkPassword { get; set; }
+
+        public string? ProxyAddress { get; set; }
+        public string? ProxyUsername { get; set; }
+        public string? ProxyPassword { get; set; }
+
+        public MuzlanClientConfig()
+        {
+            Host = DefaultHost;
+            UserAgent = DefaultUserAgent;
+        }
+    }
+}
---
+++ src/Muzlan.Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Muzlan.Api;
+
+namespace Muzlan.Cli
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var muzlan = new MuzlanClient();
+
+            await muzlan.Authenticate().ConfigureAwait(false);
+
+            Console.WriteLine("Csrf: " + muzlan.CsrfToken);
+            Console.WriteLine("Media: " + muzlan.MediaToken);
+
+            //var artists = await muzlan.GetPopularArtists().ConfigureAwait(false);
+            //var searches = await muzlan.GetRecentSearches().ConfigureAwait(false);
+            //var searchArtist = await muzlan.SearchArtists("Landser").ConfigureAwait(false);
+            var searchTracks = await muzlan.SearchTracks("Landser").ConfigureAwait(false);
+
+            Console.ReadLine();
+        }
+    }
+}
